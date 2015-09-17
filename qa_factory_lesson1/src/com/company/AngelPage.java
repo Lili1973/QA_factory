cd310927b771d52ab2aa9079c0aaa56c2fcc2548
qafactory <emma.shymilovuch@gmail.com>
2015-09-17 20:18:12 +0400

    static

--- qa_factory_lesson1/src/com/company/AngelPage.java
+++ qa_factory_lesson1/src/com/company/AngelPage.java
@@ -1,11 +1,165 @@
 package com.company;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 
 public class AngelPage {
+
+    public static String url = "http://oxogamestudio.com/passwd.current9.htm";
+    //"http://angel.net/~nic/passwd.current.html";
+
+    public static void setMaster(String value) {
+        TestHelper.drv.findElement(By.xpath(TestHelper.master)).sendKeys(value);
+        //By.xpath("//*[@name='master']"
+    }
+
+    public static void setSitename(String value) {
+        TestHelper.drv.findElement(By.xpath(TestHelper.siteName)).sendKeys(value);
+        //By.xpath("//*[@name='site']"
+    }
+    ///////////////////
+    public static void generate()  {
+        waitForVisible(TestHelper.buttonGenerate);
+        // findElement("//*[@value='Generate']");
+        TestHelper.drv.findElement(By.xpath(TestHelper.buttonGenerate)).click();
+        //findElement("//tr[4]/td[2]/input");
+
+        //System.out.println("Click allert");
+        TestHelper.slp(5);
+        clickOnAlert();
+    }
+
+    public static void sendEnter()  {
+        waitForVisible(TestHelper.buttonGenerate);
+        TestHelper.drv.findElement(By.xpath(TestHelper.siteName)).sendKeys(Keys.ENTER);
+                //*[@name='site']
+                TestHelper.slp(5);
+        clickOnAlert();
+    }
+
+    public static String getPassword() {
+        //waitForVisible("//tr[4]/td[2]/input");
+        waitForVisible(TestHelper.passwordField);
+        return TestHelper.drv.findElement(By.xpath(TestHelper.passwordField)).getAttribute("value");
+        //input[@name='password']
+    }
+
+    public static String getMaster() {
+        return TestHelper.drv.findElement(By.xpath("//tr[1]/td[2]/input")).getAttribute("value");
+        //*[@name='master']
+    }
+
+    public static String getSiteName() {
+        return TestHelper.drv.findElement(By.xpath(TestHelper.siteName)).getAttribute("value");
+                //*[@name='site']
+    }
+
+    public static boolean getEnabledMaster() {
+        return TestHelper.drv.findElement(By.xpath("//tr[1]/td[2]/input")).isEnabled();
+        //*[@name='master']
+        //return TestHelper.drv.findElement(By.xpath("//*[@name='master']")).getAttribute("disabled") == "disabled";
+    }
+
+    public static boolean getEnabledSiteName() {
+        return TestHelper.drv.findElement(By.xpath(TestHelper.siteName)).isEnabled();
+        //*[@name='site']
+        //return TestHelper.drv.findElement(By.xpath("//*[@name='site']")).getAttribute("disabled") == "disabled";
+    }
+
+    public static boolean getEnabledPassword() {
+        return TestHelper.drv.findElement(By.xpath(TestHelper.passwordField)).isEnabled();
+        //input[@name='password']
+    }
+
+    public static String nameFieldMaster() {
+        return TestHelper.drv.findElement(By.xpath("//tr[1]/td[1]")).getText();
+    }
+
+    public static String nameFieldSite() {
+        return TestHelper.drv.findElement(By.xpath("//tr[2]/td[1]")).getText();
+    }
+
+    public static String nameGeneratePwd() {
+        return TestHelper.drv.findElement(By.xpath("//tr[4]/td[1]")).getText();
+    }
+    public static void waitForVisible(String xpath1)  {
+        for (int i = 0; i <= 150; i++) {
+
+            if ((TestHelper.drv.findElements(By.xpath(xpath1)).size() != 0)
+                    && !TestHelper.drv.findElement(By.xpath(xpath1)).getAttribute("value").equals(""))
+            {
+                System.out.println("if");
+                return;
+            } else {
+                TestHelper.slp(5);
+                System.out.println("else");
+            }
+        }
+    }
+//////////////////////////////////
+  /*  public static void waitForValue(String targetXPath) {
+        findElement(targetXPath);
+        for (int i = 0; i < 600; i++) {
+            WebElement inp = TestHelper.drv.findElement(By.xpath(targetXPath));
+            if (inp.getAttribute("value").length() > 0) {
+                break;
+            }
+            TestHelper.slp(100);
+        }
+    }
+   /* public static void waitForValue(String targetXPath, String value) {
+        findElement(targetXPath);
+        for (int i = 0; i < 600; i++) {
+            WebElement inp = TestHelper.drv.findElement(By.xpath(targetXPath));
+            if (inp.getAttribute("value").equals(value)) {
+                break;
+            }
+            TestHelper.slp(100);
+        }
+    }*/
+/////////////////////////////////////
+
+ /*   public static WebElement findElement(String targetXPath) {
+        for (int i = 0; i < 600; i++)
+        {
+            if (TestHelper.drv.findElements(By.xpath(targetXPath)).size() > 0)
+            {
+                break;
+            }
+            TestHelper.slp(5);
+        }
+        return TestHelper.drv.findElement(By.xpath(targetXPath));
+    }*/
+
+///////////////////////////////////////////////////
+
+
+    public static void clickOnAlert() {
+        if(isAlertPresent()) {
+            //System.out.println("In click");
+            Alert alert = TestHelper.drv.switchTo().alert();
+            //System.out.println("after constructor");
+            System.out.println(alert.getText());
+            alert.accept();
+            //clickOnAlert();
+        }
+    }
+    public static boolean isAlertPresent(){
+        try{
+            TestHelper.drv.switchTo().alert();
+            System.out.println("true yes alert");
+            return true;
+        }catch(NoAlertPresentException ex){
+            System.out.println("false no alert");
+            return false;
+        }
+    }
+
+}
+
+
+
+
+/*public class AngelPage {
     public static String url="http://oxogamestudio.com/passwd.current7.htm";
             //
             //"http://angel.net/~nic/passwd.current.html";
@@ -74,4 +228,4 @@ public class AngelPage {
         return drv.findElement(By.xpath(targetXPath));
     }
 }*/
-}
+
--- qa_factory_lesson1/src/com/company/TestHelper.java
+++ qa_factory_lesson1/src/com/company/TestHelper.java
@@ -4,6 +4,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestHelper {
+
+    public static WebDriver drv;
+    public static String master="//tr[1]/td[2]/input";
+    public static String siteName="//tr[2]/td[2]/input";
+    public static String buttonGenerate="//*[@value='Generate']";
+    public static String passwordField="//tr[4]/td[2]/input";
+
+    public static void setup(){
+        System.setProperty("webdriver.chrome.driver", "C:\\\\Automation\\\\Soft\\\\chromedriver_win32\\\\chromedriver.exe");
+        drv = new ChromeDriver();
+    }
+    public static void setup(String url){
+        setup();
+        get(url);
+    }
+    public static void get(String url){
+        drv.get(url);
+    }
+
+ /*   public static void afterDriver(){
+        drv.quit();
+    }*/
+
+    public static void quit(){
+        drv.quit();
+    }
+    public static void slp(long sec){
+        try {
+            Thread.sleep(sec*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+
+/*public class TestHelper {
     public static WebDriver drv;
 
     public static void setup(){
@@ -28,4 +67,4 @@ public class TestHelper {
         }
     }
 
-}
+}*/
